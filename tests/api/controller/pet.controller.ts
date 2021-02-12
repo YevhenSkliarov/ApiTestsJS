086f6f381472429fac1685ec076c2ae220322891
YevhenSkliarov <eugensklyarov90@gmail.com>
2021-02-12 15:04:33 +0200

    Request Builder

--- tests/api/controller/pet.controller.ts
+++ tests/api/controller/pet.controller.ts
@@ -1,27 +1,87 @@
-import got from "got/dist/source";
 import { URLSearchParams } from "url";
+import { JsonRequest } from '../request'
 
 export class PetController {
     async getById(id: number | string) {
-        const response = await got(`http://localhost/v2/pet/${id}`)
-        return JSON.parse(response.body)
+        return (
+            await new JsonRequest()
+            .url(`http://localhost/v2/pet/${id}`)
+            .send()
+        ).body;
     }
 
     async findByTags(tags: string | string[] ) {
-        const response = await got('http://localhost/v2/pet/findByTags',{
-            searchParams: new URLSearchParams({
-                tags
-            })
-        })
-        return JSON.parse(response.body)
+        return (
+            await new JsonRequest()
+            .url('http://localhost/v2/pet/findByTags')
+            .searchParams(new URLSearchParams({tags}))
+            .send()
+        ).body
     }
 
     async findByStatus(status: string | string[] ) {
-        const response = await got('http://localhost/v2/pet/findByStatus',{
-            searchParams: new URLSearchParams({
-                status
-            })
-        })
-        return JSON.parse(response.body)
+        return (
+            await new JsonRequest()
+            .url('http://localhost/v2/pet/findByStatus')
+            .searchParams(new URLSearchParams({status}))
+            .send()
+        ).body
     }
+
+    async addNew(pet: {
+        category: {
+          id: number,
+          name: string
+        },
+        name: string,
+        photoUrls: string[],
+        tags: 
+          {
+            id: number,
+            name: string
+          }[],
+        status: string
+      }){
+        return (
+            await new JsonRequest()
+            .url(`http://localhost/v2/pet`)
+            .method('POST')
+            .body(pet)
+            .send()
+        ).body
+      }
+
+      async delete(id: number | string) {
+        return (
+            await new JsonRequest()
+            .url(`http://localhost/v2/pet/${id}`)
+            .method('DELETE')
+            .send()
+        ).body
+
+      }
+
+      async update(pet: {
+        id: number,
+        category: {
+          id: number,
+          name: string
+        },
+        name: string,
+        photoUrls: string[],
+        tags: 
+          {
+            id: number,
+            name: string
+          }[],
+        status: string
+      }){
+        return (
+            await new JsonRequest()
+            .url(`http://localhost/v2/pet`)
+            .method('PUT')
+            .body(pet)
+            .send()
+        ).body
+      }
 }
--- tests/pet.test.ts
+++ tests/pet.test.ts
@@ -4,7 +4,7 @@ import { PetController } from './api/controller/pet.controller';
 const pet = new PetController();
 
 describe('Pet', () => {
-    it('receive pet by his id',async () => {
+    it('can be received pet by his id',async () => {
         const body = await pet.getById(1);
         assert(body.id == 1,`Expected API return pet with 1`)
     });
@@ -36,4 +36,59 @@ describe('Pet', () => {
                 )
             )
     })
+
+    it('can be added, updated and deleted', async() => {
+        const petToCreate = {
+            "category": {
+                "id": 0,
+                "name": "string"
+            },
+            "name": "Cat",
+            "photoUrls": [
+                "http://test.com/image.jpg"
+            ],
+            "tags": [
+                {
+                    "id": 0,
+                    "name": "string"
+                }
+            ],
+            "status": "available"
+        }
+        const addedPet = await pet.addNew(petToCreate);
+        assert.deepEqual(addedPet, {
+            ...petToCreate,
+            id: addedPet.id
+        }, `Expected created pat to mathc data used upon creation`);
+
+        const foundAddedPet = await pet.getById(addedPet.id);
+        assert.deepEqual(foundAddedPet, {
+            ...petToCreate,
+            id: addedPet.id
+        }, `Expected found pet to math created pet`);
+
+        const newerPet = {
+            "id": addedPet.id,
+            "category": {
+                "id": 1,
+                "name": "string2"
+            },
+            "name": "Dog",
+            "photoUrls": [
+                "http://test.com/image2.jpg"
+            ],
+            "tags": [
+                {
+                    "id": 1,
+                    "name": "string2"
+                }
+            ],
+            "status": "pending"
+        }
+        const updatedPet = await pet.update(newerPet);
+        assert.deepEqual(updatedPet, newerPet, `Expected updated pet to equal data used upon updating`);
+
+        await pet.delete(addedPet.id)
+        //TODO: assert 404 error
+    });
 })
